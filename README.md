4345c283304699bbee35a95734b010d3acf6dfb1
Matthew Huckaby <matthew.huckaby@gmail.com>
2012-04-25 12:22:01 -0700

    rails new gluestick

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 gluestick
-=========
+==================
+Ruby on Rails application for sharing code between machines via network
 
-ruby on rails application for sharing code between machines via network
+
+Command history:
+==================
+rails --version
+Using Rails version 3.2.1
+
+rails new gluestick
---
+++ gluestick/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gluestick::Application.config.secret_token = '52db8af93355245e7d12a5e81e97a8731c563bca24bf02e632e998e50a1d569ab45b841dbe12e9d393f6836977ed23d107411dc760e50b3c8ffbf73e3317f65a'
