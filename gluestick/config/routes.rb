8b3b3458dbad39e20297df51779794b5be09dcef
Nishitha Ningegowda <nishitha.ningegowda@gmail.com>
2012-05-03 22:44:10 -0700

    fixed basic dir structure and added basic skeleton-divs pending

--- gluestick/config/routes.rb
+++ gluestick/config/routes.rb
@@ -1,4 +1,6 @@
 Gluestick::Application.routes.draw do
+  get "application/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +50,7 @@ Gluestick::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'application#index'
 
   # See how all your routes lay out with "rake routes"
 
