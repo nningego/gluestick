12f586b1bf71ff114564eb023d6251fd1a762d7e
Matthew Huckaby <matthew.huckaby@gmail.com>
2012-05-09 13:30:38 -0500

    paste : full roundtrip

--- gluestick/app/assets/javascripts/application.js
+++ gluestick/app/assets/javascripts/application.js
@@ -1,3 +1,17 @@
 // application.js
 //= require jquery
 //= require bootstrap.min
+
+function jqLinkTo(id){
+	var url = 'pastes/' + id
+	$.getJSON(
+		url,
+        function(jsonData){
+        	if(jsonData){
+				$('#paste_content')[0].value=jsonData.content
+            }else{
+				alert('Error retrieving paste content')
+            }
+        }
+	)
+}
