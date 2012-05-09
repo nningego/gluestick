cae6b2a4b4cfaa73ca51b16ee1ea0eac5656d5af
Matthew Huckaby <matthew.huckaby@gmail.com>
2012-05-09 01:27:04 -0500

    Removed redundant db-field

--- gluestick/db/migrate/20120508053455_create_pastes.rb
+++ gluestick/db/migrate/20120508053455_create_pastes.rb
@@ -2,7 +2,6 @@ class CreatePastes < ActiveRecord::Migration
   def change
     create_table :pastes do |t|
       t.string :content
-      t.timestamp :create_ts
 
       t.timestamps
     end
--- gluestick/db/schema.rb
+++ gluestick/db/schema.rb
@@ -14,7 +14,6 @@ ActiveRecord::Schema.define(:version => 20120508053455) do
 
   create_table "pastes", :force => true do |t|
     t.string   "content"
-    t.datetime "create_ts"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
